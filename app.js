488d887b6d918155fb25bc0c3af3994576dea291
a117171 <a1171712023@outlook.com>
2025-11-15 10:11:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-export default{
+export default {
+  async fetch(request, env, ctx) {
+    // You can view your logs in the Observability dashboard
+    console.info({ message: 'Hello World Worker received a request!' }); 
+    return new Response('Hello World!');
+  }
   const os = require('node:os');
   const fs = require('node:fs');
   const net = require('node:net');
