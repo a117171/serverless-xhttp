8397933b5ee5b1147333a366357875d821620419
a117171 <a1171712023@outlook.com>
2025-11-15 10:12:20 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ export default {
     // You can view your logs in the Observability dashboard
     console.info({ message: 'Hello World Worker received a request!' }); 
     return new Response('Hello World!');
-  }
+  };
   const os = require('node:os');
   const fs = require('node:fs');
   const net = require('node:net');
